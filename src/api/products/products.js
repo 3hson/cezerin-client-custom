f74e25862198200d5aa4e50108b38912603cd4dd
ehsan <ehsankhakian@hotmail.com>
2019-07-05 13:16:01 +0430

    initial commit

---
+++ src/api/products/products.js
@@ -0,0 +1,42 @@
+export default class Products {
+	constructor(client) {
+		this.client = client;
+		this.resourceUrl = '/products';
+	}
+
+	discounted(filter) {
+		return this.client.get(`${this.resourceUrl}/discounted`, filter);
+	}
+
+	bestSellers(filter) {
+		return this.client.get(`${this.resourceUrl}/best_sellers`, filter);
+	}
+
+	list(filter) {
+		return this.client.get(this.resourceUrl, filter);
+	}
+
+	retrieve(id, filter) {
+		return this.client.get(`${this.resourceUrl}/${id}`, filter);
+	}
+
+	create(data) {
+		return this.client.post(this.resourceUrl, data);
+	}
+
+	update(id, data) {
+		return this.client.put(`${this.resourceUrl}/${id}`, data);
+	}
+
+	delete(id) {
+		return this.client.delete(`${this.resourceUrl}/${id}`);
+	}
+
+	skuExists(productId, sku) {
+		return this.client.get(`${this.resourceUrl}/${productId}/sku`, { sku });
+	}
+
+	slugExists(productId, slug) {
+		return this.client.get(`${this.resourceUrl}/${productId}/slug`, { slug });
+	}
+}
